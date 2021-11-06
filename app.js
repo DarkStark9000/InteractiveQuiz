c447ef7901c60ac8779b707c5209b3897e1946d0
Debarshi Das <debarshi.das2019@vitstudent.ac.in>
2021-11-06 23:32:24 +0530

    🚀Revised Accessibility: Tweaked CSS

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const result = document.querySelector('.result');
 
 form.addEventListener('submit', e => {
   e.preventDefault();
+  scrollTo(0, 0);
   let score = 0;
   let userAns = [form.q1.value, form.q2.value, form.q3.value, form.q4.value, form.q5.value];
   userAns.forEach((answer, index) => {
@@ -13,8 +14,7 @@ form.addEventListener('submit', e => {
     }
   });
 
-    // Show result
-    scrollTo(0, 0);
+    // Show result 
     result.classList.remove('d-none');
 
     let out = 0;
@@ -25,7 +25,5 @@ form.addEventListener('submit', e => {
       } else {
         out++;
       }
-    }, 10);
-    
+    }, 22); 
 });
-  
